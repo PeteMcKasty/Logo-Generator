ff3322d0c4db0dc1ea78fc71592e2c4b4315e4f0
Peter McKasty <pmck25@yahoo.com>
2023-06-11 18:41:10 -0400

    added user prompt function with prompt parameters

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters:'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color (keyword or hexadecimal number):'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color (keyword or hexadecimal number):'
+        }
+    ]);
+}
